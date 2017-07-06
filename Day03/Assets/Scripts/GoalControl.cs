126e0056e665dfaefa11c6904ce8e727cbdcecda
LeeKisun <lgs61716@gmail.com>
2017-07-06 14:01:23 +0900

    03

---
+++ Day03/Assets/Scripts/GoalControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalControl : MonoBehaviour {
+    private bool ishit = false;
+    public float Goal_min = 5.0f;
+    public float Goal_max = 10.0f;
+	// Use this for initialization
+	void Start () {
+
+        float Gb = Random.Range(Goal_min, Goal_max);
+        transform.position = new Vector3(Gb, -1.0f, 0.0f);
+        
+        
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ishit = true;
+        Debug.Log("Collision");
+    }
+    private void OnGUI()
+    {
+        if(ishit)
+        {   
+            GUI.Label(new Rect(Screen.width / 2, 80, 300, 100), "성공");
+        }
+    }
+}
---
+++ Day03/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+    private float power;
+    public float PowerPlus = 100.0f;
+	// Use this for initialization
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+        {
+            power += PowerPlus * Time.deltaTime;
+            Debug.Log(power);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            Rigidbody Shot = GetComponent<Rigidbody>();
+            Shot.AddForce(new Vector3(power, power, 0));
+            power = 0.0f;
+        }
+        if(transform.position.y <-5.0f || transform.position.x>20.0f)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+	}
+}
