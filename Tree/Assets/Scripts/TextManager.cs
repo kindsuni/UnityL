2be959efe92518757b8f26c84aed0fc241a2bd00
LeeKisun <lgs61716@gmail.com>
2017-07-19 18:05:11 +0900

    update

--- Tree/Assets/Scripts/TextManager.cs
+++ Tree/Assets/Scripts/TextManager.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class TextManager : MonoBehaviour {
-    public GameObject energy;
+    public GameObject energy; //현재 에너지
+    public Text treeLevel; //나무 레벨
+    public Text clickEnergy; //클릭당 에너지
+    public Text needUp; //성장에 필요한 에너지
+    public Text clickNeed;//클릭당 에너지를 업하기 위해 필요한 에너지.
 	// Use this for initialization
 	void Start () {
-        TreeController en;
+        energy.GetComponent<Text>();
         
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
         energy.GetComponent<Text>().text ="Energy : "+ TreeController.energy +"";
-	}
+        treeLevel.text = "트리 레벨 : " + TreeController.level;
+        clickEnergy.text = "클릭당 에너지 : " + TreeController.enegyplus;
+        clickNeed.text = "클릭 : " + TreeController.clickneed;
+        needUp.text = "성장 : " + TreeController.need;
+
+    }
 }
--- Tree/Assets/Scripts/TreeController.cs
+++ Tree/Assets/Scripts/TreeController.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class TreeController : MonoBehaviour {
 
-    public static int energy = 0;
-    int enegyplus = 1;
-    int clicked = 0;
-    int j = 0;
-    int need = 100;
-    int clickneed = 0;
-    float grow = 0.1f;
+    public static int energy = 0; //현재 에너지.
+    public static int enegyplus = 1; //클릭당 에너지
+    int clicked = 0; //클릭 횟수
+    public static int need = 100; //성장 필요에너지
+    int jem; //젬
+    public static int level = 1; //트리 레벨
+    public static int clickneed = 100; //클릭당에너지 업에 필요한 에너지.
+    
+    float grow = 0.1f; //스케일 성장값.
     bool clickqust = false;
     bool treeupclick = false;
     private TreeController dondestroy;
@@ -49,51 +51,135 @@ public class TreeController : MonoBehaviour {
         }
             }
     }
-   public void tree()
+   public void tree() //트리 성장
     {
         StartCoroutine(treeup());
     }
-    public void Clickup()
+    public void Clickup() //클릭당 에너지 업
     {
         StartCoroutine(clickup());
     }
-    IEnumerator clickup()
+    IEnumerator clickup() 
     {
-        print("클릭당 에너지 :"+ enegyplus);
-        enegyplus += 5;
-        yield return null;
+        print(clickneed + "필요함");
+        if(clickneed <= energy &&!treeupclick)
+        {
+           
+            yield return StartCoroutine(CanClickup());
+        }
+        else
+        {
+            dontClickUp();
+            yield return null;
+        }
+       
     }
    IEnumerator treeup()
     {
         print(need+ " 필요함");
         if(need <= energy && !treeupclick)
         {
-            treeupclick = true;
-          yield return StartCoroutine(cantreeup());
-            yield return StartCoroutine(needup());
+           
+          yield return StartCoroutine(cantreeup()); //성장 가능.
+            
         }
         else
-        {
+        { //성장에 필요한 에너지 부족.
             dontreeup();
             yield return null;
         }
     }
-   IEnumerator cantreeup()
+    //레벨 트리 성장 (스케일, 클릭당 에너지+,트리성장에 필요한 에너지+)
+   IEnumerator cantreeup() //레벨 올리고 레벨에 맞는 능력치.
     {
         energy -= need;
-        print("남은 에너지 : "+ energy);
-        yield return null;
+        print("남은 에너지 : "+ energy);        
+        level++;
+        switch(level)
+        {
+            case 1:
+                need = 100;
+                enegyplus = 1;
+                break;
+            case 2:
+                need = 250;
+                enegyplus += 50;
+                break;
+            case 3:
+                need = 1000;
+                break;
+            case 4:
+                need = 10000;
+                break;
+            case 5:
+                need = 20000;
+                break;
+            case 6:
+                need = 25000;
+                break;
+            case 7:
+                need = 30000;
+                break;
+            case 8:
+                need = 35000;
+                break;
+            case 9:
+                need = 40000;
+                break;
+            case 10:
+                need = 45000;
+                break;
+            case 11:
+                need = 50000;
+                break;
+            case 12:
+                need = 55000;
+                break;
+            case 13:
+                need = 60000;
+                break;
+            case 14:
+                need = 65000;
+                break;
+            case 15:
+                need = 70000;
+                break;
+            case 16:
+                need = 75000;
+                break;
+            case 17:
+                need = 80000;
+                break;
+            case 18:
+                need = 90000;
+                break;
+            case 19:
+                need = 95000;
+                break;
+            case 20:
+                need = 100000;
+                break;
+        }
+       
+        
+        yield return StartCoroutine(scaleup());
     }
     void dontreeup()
     {
         print("you dont treeup");
     }
-    IEnumerator needup()
+    void dontClickUp()
     {
-        treeupclick = false;
-        need += 50;
-        yield return StartCoroutine(scaleup());
+        print("you dont ClickUp");
     }
+    IEnumerator CanClickup()
+    {
+        energy -= clickneed;
+        clickneed += 1000;
+        enegyplus += 100;
+        yield return null;
+    }
+    
 
    IEnumerator scaleup()
     {
